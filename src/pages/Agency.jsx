7ed5d3877b89adb6c978c1c3b8398604cca4ffb2
Hammad <127682041+Hammad005@users.noreply.github.com>
2025-08-31 19:24:05 +0500

    ✨ feat(agency): implement dynamic image updates on scroll
    - add image array for team members
    - update image source based on scroll progress
    - adjust scroll trigger end based on window height

--- src/pages/Agency.jsx
+++ src/pages/Agency.jsx
@@ -7,15 +7,45 @@ import { useRef } from "react";
 const Agency = () => {
   gsap.registerPlugin(ScrollTrigger);
   const imageDivRef = useRef(null);
+  const imageRef = useRef(null);
+  const imageArray = [
+    'https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/Olivier_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/Lawrence_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/HugoJoseph_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/ChantalG_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/MyleneS_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/SophieA_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/Claire_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/Michele_480X640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/MEL_480X640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/CAMILLE_480X640_2-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/MAXIME_480X640_2-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/MEGGIE_480X640_2-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/joel_480X640_3-480x640.jpg'
+  ];
+
+  console.log(window.innerHeight);
+  
   useGSAP(() => {
     gsap.to(imageDivRef.current, {
       scrollTrigger:{
         trigger: imageDivRef.current,
         markers: true,
-        start: 'top 23%',
-        end: 'top -100%',
+        start: 'top 18%',
+        end: window.innerHeight <= 700 ? "top -100%" : "top -35%",
         pin:true,
-        scrub: true,
+        scrub: 1,
+        onUpdate: (e) => {
+          let imageIndex;
+          if (e.progress < 1) {
+            imageIndex = Math.floor(e.progress * imageArray.length);
+          }else{
+            imageIndex = imageArray.length - 1
+          }
+          
+          imageRef.current.src = imageArray[imageIndex];
+        }
       }
     })
   })
@@ -23,7 +53,7 @@ const Agency = () => {
     <>
     <div className="section1">
       <div ref={imageDivRef} className="absolute h-[20vw] w-[15vw] lg:rounded-2xl rounded-lg top-36 left-[30vw] overflow-hidden">
-        <img className="h-full w-full object-cover" src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg" alt='photo1' />
+        <img ref={imageRef} className="h-full w-full object-cover" src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg" alt='photo1' />
       </div>
       <div className="relative font-[font2]">
         <div className="lg:mt-[57vh] mt-[33vh]">
@@ -34,7 +64,7 @@ const Agency = () => {
           </h1>
         </div>
         <div className="lg:pl-[35%] mt-20 px-5">
-          <p className="lg:text-[2.8vw] text-[4vw]">
+          <p className="text-[4vw] lg:text-[2.5vw]">
            &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;We're inquisitive and open-minded, and we make sure creativity
             crowds out ego from every corner. A brand is a living thing, with
             values, a personality and a story. If we ignore that, we can achieve
